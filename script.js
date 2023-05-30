6b36cc9b69cf8223e336c82cacd3af7ea9061aae
Ann Theresa Paul <102240294+anntheresapaul@users.noreply.github.com>
2023-05-30 21:27:56 +0530

    To Do List in JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("btn").addEventListener("click", myFunction);
+
+function myFunction() {
+    var item = document.querySelector("#item").value;//input value
+    if (item == "") {
+        alert("Input field is empty");
+    }
+    else {
+        var input = document.querySelector("#item");
+        if (input != "") {
+            input.value = "";
+        }
+        var list = document.querySelector("#list");//ul
+        var listtag = document.createElement("li");//li
+        var para = document.createElement("p");
+        para.innerHTML = item;
+        listtag.classList.add("mt-4", "fcol", "w-100", "ps-3", "pe-3");
+        para.classList.add("w-75", "m-0", "bg-light", "p-2");
+        listtag.appendChild(para);
+        list.append(listtag);
+        var spantag = document.createElement("span");
+        spantag.classList.add("ps-3", "w-25");
+        var delbtn = document.createElement("button");
+
+        delbtn.innerHTML = "<i class='pe-2 bi bi-trash text-white'></i> delete";
+        delbtn.setAttribute("class", "deleteme");
+        delbtn.classList.add("bg-dark", "w-100", "text-white", "p-2");
+        spantag.appendChild(delbtn);
+        listtag.appendChild(spantag);
+
+        delbtn.addEventListener('click', (e) => {
+            listtag.remove();
+        })
+    }
+}
